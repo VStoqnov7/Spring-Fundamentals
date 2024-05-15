f6a7add32384d4b3cff8e529e2a0b7c3cffac8cf
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-05-16 02:04:15 +0300

    update

---
+++ P01MobiLeLeLe/src/main/java/com/example/mobilele/web/HomeController.java
@@ -0,0 +1,47 @@
+package com.example.mobilele.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public ModelAndView getHome(ModelAndView model) {
+        model.setViewName("index");
+        return model;
+    }
+
+    @GetMapping("/users/login")
+    public ModelAndView login(ModelAndView model){
+        model.setViewName("auth-login");
+        return model;
+    }
+
+    @GetMapping("/users/register")
+    public ModelAndView register(ModelAndView model){
+        model.setViewName("auth-register");
+        return model;
+    }
+
+    @GetMapping("/offers/add")
+    public ModelAndView offersAdd(ModelAndView model){
+        model.setViewName("offer-add");
+        return model;
+    }
+
+    @GetMapping("/brands/all")
+    public ModelAndView allBrands(ModelAndView model){
+        model.setViewName("brands");
+        return model;
+    }
+
+    @GetMapping("/offers/all")
+    public ModelAndView allOffer(ModelAndView model){
+        model.setViewName("offers");
+        return model;
+    }
+}
