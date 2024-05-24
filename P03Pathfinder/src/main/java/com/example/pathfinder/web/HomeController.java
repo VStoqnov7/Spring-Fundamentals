c1b1f69d7e2db323afa3728e807ef1c5ad862bdc
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-05-24 18:39:21 +0300

    update

--- P03Pathfinder/src/main/java/com/example/pathfinder/web/HomeController.java
+++ P03Pathfinder/src/main/java/com/example/pathfinder/web/HomeController.java
@@ -1,11 +1,42 @@
 package com.example.pathfinder.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
+    @GetMapping("/register")
+    public ModelAndView register(ModelAndView model){
+        model.setViewName("register");
+        return model;
+    }
+
+    @GetMapping("/login")
+    public ModelAndView login(ModelAndView model){
+        model.setViewName("login");
+        return model;
+    }
+
+    @GetMapping("/about")
+    public ModelAndView about(ModelAndView model){
+        model.setViewName("about");
+        return model;
+    }
+
+    @GetMapping("/addRoute")
+    public ModelAndView addRoute(ModelAndView model){
+        model.setViewName("add-route");
+        return model;
+    }
+
+    @GetMapping("/profile")
+    public ModelAndView profile(ModelAndView model){
+        model.setViewName("profile");
+        return model;
+    }
 
 }
