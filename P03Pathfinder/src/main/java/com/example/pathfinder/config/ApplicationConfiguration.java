db5f3cc63c575cf4bbeb1f3e1659293199e9d722
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-05-24 21:49:37 +0300

    update

--- P03Pathfinder/src/main/java/com/example/pathfinder/config/ApplicationConfiguration.java
+++ P03Pathfinder/src/main/java/com/example/pathfinder/config/ApplicationConfiguration.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.client.RestTemplate;
+
+
 
 @Configuration
 public class ApplicationConfiguration {
---
+++ P03Pathfinder/src/main/java/com/example/pathfinder/models/user/CurrentUser.java
@@ -0,0 +1,33 @@
+package com.example.pathfinder.models.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Component
+@SessionScope
+public class CurrentUser {
+
+    private String username;
+    private boolean loggedIn;
+    private boolean isAdmin;
+
+    public void clear() {
+        this.username = null;
+        this.loggedIn = false;
+        this.isAdmin = false;
+    }
+
+    public boolean isAnonymous() {
+        return !isLoggedIn();
+    }
+
+    public boolean isUserAdmin() {
+        return !isAdmin();
+    }
+}
